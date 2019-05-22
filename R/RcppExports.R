df14162eba5a4e2c7f6f61c0ec4987e9b14d0eb4
EpiSlim <lotfi.slim91@gmail.com>
2019-05-22 16:15:55 +0200

    finalized documentation of Rcpp functions

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -189,7 +189,61 @@ quadHSIC <- function(K) {
     .Call(`_kernelPSI_quadHSIC`, K)
 }
 
-sampleH <- function(A, initial, n_replicates, mu = 0.0, sigma = 1.0, n_iter = 1.0e+5, burn_in = 1.0e+4) {
+#' samples within the acceptance region defined by the kernel selection event
+#'
+#' To approximate the distribution of the test statistics, we iteratively
+#' sample replicates of the response in order to generate replicates
+#' of the test statistics. The response replicates are iteratively sampled
+#' within the acceptance region of the selection event. The goal of the
+#' constrained sampling is to obtain a valid post-selection distribution of
+#' the test statistic. To perform the constrained sampling, we develop a hit-and-run
+#' sampler based on the hypersphere directions algorithm (see references).
+#'
+#' Given the iterative nature of the sampler, a large number of
+#' \code{n_replicates} and \code{burn_in} iterations is needed to correctly
+#' approximate the test statistics distributions.
+#'
+#' For high-dimensional responses, and depending on the initialization, the
+#' sampler may not scale well to generate tens of thousands of replicates
+#' because of an intermediate rejection sampling step.
+#'
+#' @param A list of matrices modeling the quadratic constraints of the
+#' selection event
+#' @param initial initialization sample. This sample must belong to the
+#' acceptance region given by \code{A}. In practice, this parameter is set
+#' to the outcome of the original dataset.
+#' @param n_replicates total number of replicates to be generated
+#' @param mu mean of the outcome
+#' @param sigma standard deviation of the outcome
+#' @param n_iter maxmimum number of rejections for the parameter \eqn{\lambda}
+#' in a single iteration
+#' @param burn_in number of burn-in iterations
+#'
+#' @return a matrix with \code{n_replicates} columns where each column
+#' contains a sample within the acceptance region
+#'
+#' @references Berbee, H. C. P., Boender, C. G. E., Rinnooy Ran, A. H. G.,
+#' Scheffer, C. L., Smith, R. L., & Telgen, J. (1987). Hit-and-run algorithms
+#' for the identification of nonredundant linear inequalities. Mathematical
+#' Programming, 37(2), 184–207.
+#'
+#' @references Belisle, C. J. P., Romeijn, H. E., & Smith, R. L. (2016).
+#' HIT-AND-RUN ALGORITHMS FOR GENERATING MULTIVARIATE DISTRIBUTIONS,
+#' 18(2), 255–266.
+#'
+#' @examples
+#' n <- 30
+#' p <- 20
+#' K <- replicate(5, matrix(rnorm(n*p), nrow = n, ncol = p), simplify = FALSE)
+#' K <-  sapply(K, function(X) return(X %*% t(X) / dim(X)[2]), simplify = FALSE)
+#' L <- matrix(rnorm(n*p), nrow = n, ncol = p)
+#' L <-  L %*% t(L) / p
+#' selection <- FOHSIC(K, L, 2)
+#' constraintQ <- forwardQ(K, select = selection)
+#' samples <- sampleH(A = constraintQ, initial = rep(0, n),
+#'                    n_replicates = 100, burn_in = 20)
+#' @export
+sampleH <- function(A, initial, n_replicates, mu = 0.0, sigma = 1.0, n_iter = 1.0e+5, burn_in = 1.0e+3) {
     .Call(`_kernelPSI_sampleH`, A, initial, n_replicates, mu, sigma, n_iter, burn_in)
 }
 
--- src/sampleH.cpp
+++ src/sampleH.cpp
@@ -7,42 +7,62 @@ using namespace Rcpp;
 
 //' samples within the acceptance region defined by the kernel selection event
 //'
-//' Similarly to the fixed variant, the adaptive selection of the 
-//' kernels in a forward fashion can also be modeled with a set of 
-//' quadratic constraints. The constraints for adaptive selection can be split
-//' into two subsets. The first subset encodes the order of selection of the 
-//' kernels, while the second subset encodes the selection of the number of the
-//' kernels. The two subsets are equally sized (\code{length(K) - 1}) and are 
-//' sequentially included in the output list. 
-//' 
-//' @param K list kernel similarity matrices
-//' @param select integer vector containing the order of selection of the kernels 
-//' in \code{K}. Typically, the \code{selection} field of the output of 
-//' \code{\link{FOHSIC}}. 
-//' @param n number of selected kernels. Typically, the \code{n} field of the 
-//' output of \code{\link{adaFOHSIC}}. 
+//' To approximate the distribution of the test statistics, we iteratively
+//' sample replicates of the response in order to generate replicates
+//' of the test statistics. The response replicates are iteratively sampled
+//' within the acceptance region of the selection event. The goal of the
+//' constrained sampling is to obtain a valid post-selection distribution of
+//' the test statistic. To perform the constrained sampling, we develop a hit-and-run
+//' sampler based on the hypersphere directions algorithm (see references).
 //'
-//' @return list of matrices modeling the quadratic constraints of the
-//' adaptive selection event
-//' 
-//' @references Loftus, J. R., & Taylor, J. E. (2015). Selective inference in 
-//' regression models with groups of variables.
+//' Given the iterative nature of the sampler, a large number of
+//' \code{n_replicates} and \code{burn_in} iterations is needed to correctly
+//' approximate the test statistics distributions.
+//'
+//' For high-dimensional responses, and depending on the initialization, the
+//' sampler may not scale well to generate tens of thousands of replicates
+//' because of an intermediate rejection sampling step.
+//'
+//' @param A list of matrices modeling the quadratic constraints of the
+//' selection event
+//' @param initial initialization sample. This sample must belong to the
+//' acceptance region given by \code{A}. In practice, this parameter is set
+//' to the outcome of the original dataset.
+//' @param n_replicates total number of replicates to be generated
+//' @param mu mean of the outcome
+//' @param sigma standard deviation of the outcome
+//' @param n_iter maxmimum number of rejections for the parameter \eqn{\lambda}
+//' in a single iteration
+//' @param burn_in number of burn-in iterations
+//'
+//' @return a matrix with \code{n_replicates} columns where each column
+//' contains a sample within the acceptance region
+//'
+//' @references Berbee, H. C. P., Boender, C. G. E., Rinnooy Ran, A. H. G.,
+//' Scheffer, C. L., Smith, R. L., & Telgen, J. (1987). Hit-and-run algorithms
+//' for the identification of nonredundant linear inequalities. Mathematical
+//' Programming, 37(2), 184–207.
+//'
+//' @references Belisle, C. J. P., Romeijn, H. E., & Smith, R. L. (2016).
+//' HIT-AND-RUN ALGORITHMS FOR GENERATING MULTIVARIATE DISTRIBUTIONS,
+//' 18(2), 255–266.
 //'
 //' @examples
-//' n <- 50
+//' n <- 30
 //' p <- 20
-//' K <- replicate(8, matrix(rnorm(n*p), nrow = n, ncol = p), simplify = FALSE)
+//' K <- replicate(5, matrix(rnorm(n*p), nrow = n, ncol = p), simplify = FALSE)
 //' K <-  sapply(K, function(X) return(X %*% t(X) / dim(X)[2]), simplify = FALSE)
 //' L <- matrix(rnorm(n*p), nrow = n, ncol = p)
 //' L <-  L %*% t(L) / p
-//' adaS <- adaFOHSIC(K, L)
-//' listQ <- adaQ(K, select = adaS[["selection"]], n = adaS[["n"]])
+//' selection <- FOHSIC(K, L, 2)
+//' constraintQ <- forwardQ(K, select = selection)
+//' samples <- sampleH(A = constraintQ, initial = rep(0, n),
+//'                    n_replicates = 100, burn_in = 20)
 //' @export
-
 // [[Rcpp::export]]
 arma::mat sampleH(arma::field<arma::mat> A, NumericVector initial, int n_replicates,
                   double mu = 0.0, double sigma = 1.0,
-                  int n_iter = 1.0e+5, int burn_in = 1.0e+4)
+                  int n_iter = 1.0e+5, int burn_in = 1.0e+3)
 {
 
     int n = initial.size();
