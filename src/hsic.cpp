bace700993eb0d6cf5682860a685b204a3604245
EpiSlim <lotfi.slim91@gmail.com>
2019-08-15 18:35:20 +0200

    reduced the complexity in HSIC and quadHSIC

--- src/hsic.cpp
+++ src/hsic.cpp
@@ -36,19 +36,24 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 double HSIC(arma::mat K, arma::mat L)
 {
-
+    
     double n = (double) K.n_rows;
-
+    
     arma::mat KK = K - arma::diagmat(K);
     arma::mat LL = L - arma::diagmat(L);
-    arma::mat KL = KK * LL;
-
-    double hsic = (arma::trace(KL) + arma::accu(KK) * arma::accu(LL) / ((n - 1) * (n - 2))
-                   - 2 * arma::accu(KL) / (n - 2)) / (n * (n - 3));
-
+    arma::colvec onecol = arma::ones(n);
+    arma::rowvec onerow = onecol.t();
+    
+    double traceKL = 0;
+    for (int i = 0; i < n; i++) {
+        traceKL += arma::as_scalar(KK.row(i) * LL.col(i)); 
+    }
+    
+    double hsic = (traceKL + arma::accu(KK) * arma::accu(LL) / ((n - 1) * (n - 2))
+                       - 2 * arma::dot(onerow * KK, LL * onecol) / (n - 2)) / (n * (n - 3));
+    
     return hsic;
 }
-
 
 //' Determines the quadratic form of the HSIC unbiased estimator
 //'
@@ -72,13 +77,15 @@ double HSIC(arma::mat K, arma::mat L)
 arma::mat quadHSIC(arma::mat K)
 {
     double n = (double) K.n_rows;
-
-     arma::mat Q = K - arma::diagmat(K) +
-                  arma::accu(K - arma::diagmat(K)) *
-                  (arma::ones(size(K)) - arma::eye(size(K)))/ ((n - 1) * (n - 2)) -
-                  (2 / (n -2)) * (arma::ones(size(K)) * K - arma::diagmat(arma::ones(size(K)) * K) -
-                                  arma::ones(size(K)) * arma::diagmat(K) +
-                                  arma::diagmat(arma::ones(size(K)) * arma::diagmat(K)));
-
+    arma::colvec onecol = arma::ones(n);
+    arma::rowvec onerow = onecol.t();
+    
+    arma::mat Q = K - arma::diagmat(K) +
+        arma::accu(K - arma::diagmat(K)) *
+        (arma::ones(size(K)) - arma::eye(size(K)))/ ((n - 1) * (n - 2)) -
+        (2 / (n -2)) * (onecol * (onerow * K) - arma::diagmat(onecol * (onerow * K)) -
+        onecol * (onerow * arma::diagmat(K)) +
+        arma::diagmat(onecol * (onerow * arma::diagmat(K))));
+    
     return Q;
 }
