730d4ed148b91664a87be9b9cddd98962775d746
EpiSlim <lotfi.slim91@gmail.com>
2019-08-26 10:51:11 +0200

    updated the CUDA sampler

--- src/sampleC.cu
+++ src/sampleC.cu
@@ -1,14 +1,17 @@
 #define ARMA_ALLOW_FAKE_GCC
 
 #define VIENNACL_WITH_CUDA
-//#define VIENNACL_WITH_OPENCL
 #define VIENNACL_WITH_OPENMP
 #define VIENNACL_WITH_ARMADILLO 1
+
+#include <ctime>
+#include <ratio>
+#include <chrono>
+using namespace std::chrono;
 
 #include <RcppArmadillo.h>
 #include <RcppArmadilloExtensions/sample.h>
 using namespace Rcpp;
-
 
 // ViennaCL headers
 #include "viennacl/vector.hpp"
@@ -16,6 +19,7 @@ using namespace Rcpp;
 #include "viennacl/forwards.h"
 #include "viennacl/matrix_proxy.hpp"
 #include "viennacl/linalg/inner_prod.hpp"
+#include "viennacl/linalg/maxmin.hpp"
 
 
 // [[Rcpp::plugins(cpp11)]]
@@ -32,55 +36,68 @@ arma::mat sampleC(arma::field<arma::mat> A, NumericVector initial, int n_replica
     arma::vec candidateO(n), candidateQ(n), candidateN = Rcpp::as<arma::vec>(wrap(pnorm(initial, mu, sigma)));
 
     // Randomly sample in the sphere unit
+    arma::vec thetaV(n);
     arma::mat theta(n, n_replicates + burn_in, arma::fill::randn);
     theta = arma::normalise(theta, 2, 0);
 
     // Rejection sampling
-    arma::vec cdt(A.n_elem);
     arma::vec::iterator l;
     arma::vec boundA, boundB;
-    arma::mat matA(n, n*A.n_elem);
+    arma::mat matA(n*A.n_elem, n);
     for (int r = 0; r < A.n_elem; ++r){
-        matA(0, n*r, size(A(r))) = A(r); // Regrouping the list of matrices in a single GPU matrix
+        matA(n*r, 0, size(A(r))) = A(r); // Regrouping the list of matrices in a single GPU matrix
     }
 
     // Declaring GPU objects
-    viennacl::vector<double> vectorCL(n), resultCL(n);
-    viennacl::matrix<double, viennacl::column_major> matrixCL(n, n*A.n_elem);
+    viennacl::matrix_base<double> baseCL(A.n_elem, n, true);
+    viennacl::vector<double> vectorCL(n), resultCL(n*A.n_elem), cdtCL(A.n_elem);
+    viennacl::matrix<double, viennacl::column_major> matrixCL(n*A.n_elem, n);
+    viennacl::matrix<double, viennacl::row_major> prodCL(A.n_elem, n);
     copy(matA, matrixCL);
 
     int r;
     for (int s = 0; s < (n_replicates + burn_in); ++s)
     {
+
+
         candidateO = candidateN;
+        thetaV = theta.col(s);
 
-        boundA = -(candidateO/theta.col(s));
-        boundB = (1 - candidateO)/theta.col(s);
+        boundA = -(candidateO/thetaV);
+        boundB = (1 - candidateO)/thetaV;
 
-        double leftQ = std::max(boundA.elem(arma::find(theta.col(s) > 0)).max(),
-                                boundB.elem(arma::find(theta.col(s) < 0)).max());
-        double rightQ = std::min(boundA.elem(arma::find(theta.col(s) < 0)).min(),
-                                 boundB.elem(arma::find(theta.col(s) > 0)).min());
+        double leftQ = std::max(boundA.elem(arma::find(thetaV > 0)).max(),
+                                boundB.elem(arma::find(thetaV < 0)).max());
+        double rightQ = std::min(boundA.elem(arma::find(thetaV < 0)).min(),
+                                 boundB.elem(arma::find(thetaV > 0)).min());
+
 
         for (int iter = 0; iter < n_iter; ++iter)
         {
             if (iter == n_iter) stop("The quadratic constraints cannot be satisfied");
-            double lambda = runif(1, leftQ, rightQ)[0];
-            candidateN = candidateO + lambda * theta.col(s);
-            candidateQ = Rcpp::as<arma::vec>(wrap(qnorm(as<NumericVector>(wrap(candidateN)), mu, sigma)));
-            viennacl::copy(candidateQ, vectorCL);
-            for(l = cdt.begin(), r = 0; l != cdt.end(); ++l, ++r)
-            {
-                resultCL = viennacl::linalg::prod(viennacl::project(matrixCL, viennacl::range(0, n), viennacl::range(n*r, n*(r+1))), vectorCL);
-                *l = viennacl::linalg::inner_prod(vectorCL, resultCL);
 
-            }
-            if (all(cdt >= 0)) {
+            double lambda = runif(1, leftQ, rightQ)[0];
+            candidateN = candidateO + lambda * thetaV;
+            candidateQ = Rcpp::as<arma::vec>(wrap(qnorm(as<NumericVector>(wrap(candidateN)), mu, sigma)));
+
+            viennacl::copy(candidateQ, vectorCL);
+            resultCL = viennacl::linalg::prod(matrixCL, vectorCL);
+
+            baseCL = viennacl::matrix_base<double> (resultCL.handle(),
+                                                    A.n_elem, 0, 1, A.n_elem,
+                                                    n, 0, 1, n,
+                                                    true);
+            prodCL = baseCL;
+            cdtCL = viennacl::linalg::prod(prodCL, vectorCL);
+
+            if (viennacl::linalg::min(cdtCL) >= 0) {
                 qsamples.col(s) = candidateQ;
                 break;
             }
 
         }
+
+
     }
 
     return qsamples.cols(burn_in, n_replicates + burn_in - 1);
