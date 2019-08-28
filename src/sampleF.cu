e7f8c1751137a740c470b0d2062fffbc511978a9
EpiSlim <lotfi.slim91@gmail.com>
2019-08-28 11:15:11 +0200

    local changes

---
+++ src/sampleF.cu
@@ -0,0 +1,115 @@
+#define ARMA_ALLOW_FAKE_GCC
+
+#define VIENNACL_WITH_CUDA
+#define VIENNACL_WITH_OPENMP
+#define VIENNACL_WITH_ARMADILLO 1
+
+// Timing headers
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include <cstdlib>
+#define USECPSEC 1000000ULL
+
+long long otime_usec(unsigned long long start){
+
+  timeval tv;
+  gettimeofday(&tv, 0);
+  return ((tv.tv_sec*USECPSEC)+tv.tv_usec)-start;
+}
+
+#include <RcppArmadillo.h>
+//#include <RcppArmadilloExtensions/sample.h>
+using namespace Rcpp;
+
+// ViennaCL headers
+#include "viennacl/vector.hpp"
+#include "viennacl/matrix.hpp"
+#include "viennacl/forwards.h"
+#include "viennacl/matrix_proxy.hpp"
+#include "viennacl/linalg/inner_prod.hpp"
+#include "viennacl/linalg/maxmin.hpp"
+
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+
+arma::fmat sampleF(arma::field<arma::fmat> A, NumericVector initial, int n_replicates,
+                  float mu = 0.0, float sigma = 1.0,
+                  int n_iter = 1.0e+5, int burn_in = 1.0e+3)
+{
+
+    int n = initial.size();
+    arma::fmat qsamples(n, n_replicates + burn_in, arma::fill::zeros);
+    arma::fmat candidates(n, n_replicates + burn_in + 1, arma::fill::zeros);
+    arma::fvec candidateO(n), candidateQ(n), candidateN = Rcpp::as<arma::fvec>(wrap(pnorm(initial, mu, sigma)));
+
+    // Randomly sample in the sphere unit
+    arma::fvec thetaV(n);
+    arma::fmat theta(n, n_replicates + burn_in, arma::fill::randn);
+    theta = arma::normalise(theta, 2, 0);
+
+    // Rejection sampling
+    arma::fvec::iterator l;
+    arma::fvec boundA, boundB, cdt(A.n_elem), leftV(n), rightV(n);
+    arma::fmat matA(n*A.n_elem, n);
+    for (int r = 0; r < A.n_elem; ++r){
+        matA(n*r, 0, size(A(r))) = A(r); // Regrouping the list of matrices in a single GPU matrix
+    }
+
+    // Declaring GPU objects
+    viennacl::matrix_base<float> baseCL(A.n_elem, n, true);
+    viennacl::vector<float> vectorCL(n), resultCL(n*A.n_elem), cdtCL(A.n_elem);
+    viennacl::matrix<float, viennacl::column_major> matrixCL(n*A.n_elem, n);
+    viennacl::matrix<float, viennacl::row_major> prodCL(A.n_elem, n);
+    copy(matA, matrixCL);
+
+    int r;
+    for (int s = 0; s < (n_replicates + burn_in); ++s)
+    {
+
+
+        candidateO = candidateN;
+        thetaV = theta.col(s);
+
+        boundA = -(candidateO/thetaV);
+        boundB = (1 - candidateO)/thetaV;
+
+        //long long dt = otime_usec(0);
+        float leftQ = std::max(boundA.elem(arma::find(thetaV > 0)).max(),
+                                boundB.elem(arma::find(thetaV < 0)).max());
+        float rightQ = std::min(boundA.elem(arma::find(thetaV < 0)).min(),
+                                 boundB.elem(arma::find(thetaV > 0)).min());
+        //dt = otime_usec(dt);
+        //std::cout << "bounds time: " << dt/(float)USECPSEC << "s" << std::endl;
+
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            if (iter == n_iter) stop("The quadratic constraints cannot be satisfied");
+
+            float lambda = runif(1, leftQ, rightQ)[0];
+            candidateN = candidateO + lambda * thetaV;
+            candidateQ = Rcpp::as<arma::fvec>(wrap(qnorm(as<NumericVector>(wrap(candidateN)), mu, sigma)));
+
+            viennacl::copy(candidateQ, vectorCL);
+            resultCL = viennacl::linalg::prod(matrixCL, vectorCL);
+            baseCL = viennacl::matrix_base<float> (resultCL.handle(),
+                                                    A.n_elem, 0, 1, A.n_elem,
+                                                    n, 0, 1, n,
+                                                    true);
+            prodCL = baseCL;
+            cdtCL = viennacl::linalg::prod(prodCL, vectorCL);
+            viennacl::copy(cdtCL, cdt);
+
+            if (all(cdt >= 0)) {
+                qsamples.col(s) = candidateQ;
+                break;
+            }
+
+        }
+
+
+    }
+
+    return qsamples.cols(burn_in, n_replicates + burn_in - 1);
+}
