1acef4d671b9919b1eca6b0a2be5dfe57609c44d
Lotfi SLIM <lslim@thalassa.cm.cluster>
2020-05-04 16:48:45 +0200

    The two modes of compilation are working now

---
+++ src/statC_cpu.cpp
@@ -0,0 +1,19 @@
+#define ARMA_ALLOW_FAKE_GCC
+
+#include <RcppArmadillo.h>
+// #include <RcppArmadilloExtensions/sample.h>
+using namespace Rcpp;
+
+#include "hsic.h"
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+
+double statC(arma::vec sample, arma::mat replicates,
+              arma::field<arma::mat> K) {
+
+  stop("Trying to access a GPU function");
+
+  return 0.0;
+}
+
