50167a79d875883b4abab1f92830fa4cbb0bcf47
EpiSlim <lotfi.slim91@gmail.com>
2020-05-05 21:20:59 +0200

    added a CPU version of statC

--- src/statC_cpu.cpp
+++ src/statC_cpu.cpp
@@ -8,12 +8,27 @@ using namespace Rcpp;
 
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+double statC2(arma::vec sample, arma::mat replicates, arma::field<arma::mat> K) {
 
-double statC(arma::vec sample, arma::mat replicates,
-              arma::field<arma::mat> K) {
+  int n = sample.size();
+  arma::mat Ksum(n, n, arma::fill::zeros), HSICmat(n, n, arma::fill::zeros);
 
-  //stop("Trying to access a GPU function");
+  // Computing the quadratic form of the selected kernels
+  for (int r = 0; r < K.n_elem; ++r) {
+    Ksum += K(r);
+  }
+  HSICmat = quadHSIC(Ksum);
 
-  return 0.0;
+  // Computing the statistics of the replicates
+  arma::vec stat =
+      arma::ones(replicates.n_rows).t() * ((Ksum * replicates) % replicates);
+
+  // Computing the statistic of original sample
+  double statS = sample.t() * Ksum * sample;
+
+  // Compute p-value
+  double pvalue = arma::sum(stat > statS) / (double)replicates.n_cols;
+
+  return pvalue;
 }
-
